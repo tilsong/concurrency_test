2bc7f449cd1f314ea7866810bd165a3117d81550
tilsong <cnsong0229@gmail.com>
2023-06-27 00:28:31 +0900

    edit bug (Lock by Mysql)

--- src/main/java/com/example/stack/service/OptimisticLockStockService.java
+++ src/main/java/com/example/stack/service/OptimisticLockStockService.java
@@ -3,6 +3,7 @@ package com.example.stack.service;
 import com.example.stack.domain.Stock;
 import com.example.stack.repository.StockRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class OptimisticLockStockService {
@@ -13,6 +14,7 @@ public class OptimisticLockStockService {
         this.stockRepository = stockRepository;
     }
 
+    @Transactional
     public void decrease(Long id, Long quantity) {
         Stock stock = stockRepository.findByIdWithOptimisticLock(id);
         stock.decrease(quantity);
